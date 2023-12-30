bd9a454ce8a339660157b3108ce88520a6a6dcce
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2023-12-30 16:30:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const lodash = require("lodash");
+
+const obj1 = { name: "sanjay", age: 40 };
+const obj2 = { age: 40, name: "sanjay" };
+
+// compare above two objects and find whether they are equal
+
+console.log(obj1 === obj2);
+
+console.log(lodash.isEqual(obj1, obj2));
